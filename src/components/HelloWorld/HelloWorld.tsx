ae6d0e5903eaa28bbbc92b7c71a4d296d065708a
Robbie McClellan <rlm8291@gmail.com>
2021-05-11 15:47:11 -0400

    Setup Navigation Animations + Update Visual Sizing For Navigation

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -3,5 +3,9 @@ import { h } from 'preact'
 import styles from './style.module.css'
 
 export default function HelloWorld() {
-  return <div className={styles.text}>Hello Preact!!!</div>
+  return (
+    <div onClick={() => console.log('tag')} className={styles.text}>
+      Hello Preact!!!
+    </div>
+  )
 }
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,5 +1,7 @@
 // Navigation Component
 import { h } from 'preact'
+import { useState } from 'preact/hooks'
+import cx from 'classnames'
 
 // Import Styling
 import styles from './style.module.css'
@@ -8,26 +10,34 @@ import styles from './style.module.css'
 import IteeaLogo from '../../../public/iteea.ico'
 
 export default function Navigation() {
+  const [display, setDisplay] = useState(false)
+  const menuButtonClick = () => setDisplay(!display)
+
   return (
     <nav className={styles.navigation}>
       <div className={styles.logo}>
         <img src={IteeaLogo} alt='Iteea Logo' />
         <p>ITEEA Stel eTool</p>
-        <div className={styles.menu}>
+        <div
+          className={cx(styles.menu, { [styles.animateBtn]: display })}
+          onClick={menuButtonClick}
+        >
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
-      <section className={styles.navLinks}>
+      <section
+        className={cx(styles.navLinks, { [styles.animateNav]: display })}
+      >
         <ul>
-          <li>
+          <li onClick={menuButtonClick}>
             <a href='/'>Home</a>
           </li>
-          <li>
+          <li onClick={menuButtonClick}>
             <a href='/about'>About Page</a>
           </li>
-          <li>
+          <li onClick={menuButtonClick}>
             <a href='/hello'>Hello</a>
           </li>
         </ul>
